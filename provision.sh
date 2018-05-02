f1a88105239be19748421a78d28f04e18f35ff43
Aditya Hadiwijaya <hadiwijaya.aditya@gmail.com>
2018-05-02 13:38:34 +0700

    sudo all command

--- provision.sh
+++ provision.sh
@@ -10,41 +10,41 @@ internal_interface=$(echo "$interfaces" | cut -d, -f 2)
 
 # DOCKER
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y docker.io curl unzip virtualbox-guest-utils
+sudo apt-get update
+sudo apt-get install -y docker.io curl unzip virtualbox-guest-utils
 
 # ROUTING/NAT
 modprobe iptable_nat
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo "net.ipv4.ip_forward=1" > /etc/sysctl.d/99-ip_forward.conf
-mkdir /etc/iptables
+sudo mkdir /etc/iptables
 INTERNAL_INTERFACE="$internal_interface" NAT_INTERFACE="$nat_interface" \
     envsubst '$NAT_INTERFACE $INTERNAL_INTERFACE' < /files/iptables.envsubst.tpl > /etc/iptables/rules.v4
-apt-get install -y iptables-persistent
+sudo apt-get install -y iptables-persistent
 
 # DNSMASQ
 INTERNAL_INTERFACE="$internal_interface" INTERNAL_IP="$internal_ip" \
     envsubst '$INTERNAL_IP $INTERNAL_INTERFACE' < /files/dnsmasq.conf.envsubst.tpl > /etc/dnsmasq.conf
-cp /files/dnsmasq.service /etc/systemd/system
-systemctl enable /etc/systemd/system/dnsmasq.service
-systemctl start dnsmasq.service
+sudo cp /files/dnsmasq.service /etc/systemd/system
+sudo systemctl enable /etc/systemd/system/dnsmasq.service
+sudo systemctl start dnsmasq.service
 
 # MATCHBOX
-mkdir -p /etc/matchbox
-cp /certs/* /etc/matchbox
-mkdir -p /var/lib/matchbox/scripts
-cp /files/get-coreos /var/lib/matchbox/scripts
-/var/lib/matchbox/scripts/get-coreos stable 1465.8.0 /var/lib/matchbox/assets
+sudo mkdir -p /etc/matchbox
+sudo cp /certs/* /etc/matchbox
+sudo mkdir -p /var/lib/matchbox/scripts
+sudo cp /files/get-coreos /var/lib/matchbox/scripts
+sudo /var/lib/matchbox/scripts/get-coreos stable 1465.8.0 /var/lib/matchbox/assets
 
-cp /files/matchbox.service /etc/systemd/system
-systemctl enable /etc/systemd/system/matchbox.service
-systemctl start matchbox.service
+sudo cp /files/matchbox.service /etc/systemd/system
+sudo systemctl enable /etc/systemd/system/matchbox.service
+sudo systemctl start matchbox.service
 
 # ubuntu user setup
-cp /files/id_rsa* /home/ubuntu/.ssh
-chown ubuntu:ubuntu /home/ubuntu/.ssh/id_rsa*
-chmod 600 /home/ubuntu/.ssh/id_rsa
-usermod -G docker ubuntu
+sudo cp /files/id_rsa* /home/ubuntu/.ssh
+sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/id_rsa*
+sudo chmod 600 /home/ubuntu/.ssh/id_rsa
+sudo usermod -G docker ubuntu
 
 # TERRAFORM
 temp_dir=$(mktemp -d)
@@ -52,14 +52,14 @@ curl --silent -L https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.
 curl --silent -L https://github.com/coreos/terraform-provider-matchbox/releases/download/v0.2.2/terraform-provider-matchbox-v0.2.2-linux-amd64.tar.gz -o "$temp_dir/terraform-provider-matchbox.tar.gz"
 pushd "$temp_dir"
 unzip terraform.zip
-cp terraform /usr/local/bin
+sudo cp terraform /usr/local/bin
 tar xzvf terraform-provider-matchbox.tar.gz
-cp terraform-provider-matchbox-v0.2.2-linux-amd64/terraform-provider-matchbox /usr/local/bin
+sudo cp terraform-provider-matchbox-v0.2.2-linux-amd64/terraform-provider-matchbox /usr/local/bin
 popd
-rm -rf "$temp_dir"
-mkdir -p /home/ubuntu/.matchbox/infra-local
-cp /certs/* /home/ubuntu/.matchbox/infra-local
-chown -R ubuntu:ubuntu /home/ubuntu/terraform
+sudo rm -rf "$temp_dir"
+sudo mkdir -p /home/ubuntu/.matchbox/infra-local
+sudo cp /certs/* /home/ubuntu/.matchbox/infra-local
+sudo chown -R ubuntu:ubuntu /home/ubuntu/terraform
 
 # SERVICES CHECK
 for service in matchbox dnsmasq; do
