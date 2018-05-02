92ab3461f72ab2cacd7ec4ac17b27d31d42f2ad0
Aditya Hadiwijaya <hadiwijaya.aditya@gmail.com>
2018-05-02 16:32:03 +0700

    redo and define ip

--- provision.sh
+++ provision.sh
@@ -2,6 +2,12 @@
 
 set -x
 
+internal_ip=192.168.209.137
+
+interfaces=$(ifconfig -a | perl scripts/parse_ifconfig.pl "$internal_ip")
+nat_interface=$(echo "$interfaces" | cut -d, -f 1)
+internal_interface=$(echo "$interfaces" | cut -d, -f 2)
+
 # DOCKER
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
